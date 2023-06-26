72aef7cdcd3b915a2a666df5dda5e0e0ab67ea50
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2023-06-26 23:07:10 +0900

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//ページトップへ戻る
+var $pageTop = $('.pagetop');
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+    $pageTop.fadeIn();
+  } else {
+    $pageTop.fadeOut();
+  }
+});
+$pageTop.on('click', function () {
+  $('body,html').animate({
+  scrollTop: 0
+  }, 300);
+  return false;
+});
